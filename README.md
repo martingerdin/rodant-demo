e841f6d0fc945aed75e1cf1f1eb096c4b511d907
Martin Gerdin Warnberg <martingerdin@gmail.com>
2020-11-02 11:00:17 +0100

    Add first demo build

---
+++ README.md
@@ -0,0 +1,2 @@
+# A live rodant demo
+Please check out [our main github page](https://github.com/tracits/rodant)
---
+++ precache-manifest.13bb93c0842d8da282ba2040677851f9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2348ba46f6550fcba1fc7d9080d36e20",
+    "url": "/rodant-demo/index.html"
+  },
+  {
+    "revision": "278a116b066a9dc2cfda",
+    "url": "/rodant-demo/static/css/3.d0123ed2.chunk.css"
+  },
+  {
+    "revision": "51be2a039c0dafd02ec7",
+    "url": "/rodant-demo/static/css/main.d2d868ee.chunk.css"
+  },
+  {
+    "revision": "786ab8091b7c6b3f8788",
+    "url": "/rodant-demo/static/js/0.4b691af5.chunk.js"
+  },
+  {
+    "revision": "278a116b066a9dc2cfda",
+    "url": "/rodant-demo/static/js/3.feace261.chunk.js"
+  },
+  {
+    "revision": "51be2a039c0dafd02ec7",
+    "url": "/rodant-demo/static/js/main.b1cb1327.chunk.js"
+  },
+  {
+    "revision": "c464630e87b2045e2f61",
+    "url": "/rodant-demo/static/js/runtime-main.864f8604.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/rodant-demo/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/rodant-demo/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/rodant-demo/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/rodant-demo/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/rodant-demo/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
