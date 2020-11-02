dcabf2d9abd0e20018bd36aa3de108c7f881c081
Martin Gerdin Warnberg <martingerdin@gmail.com>
2020-11-02 11:12:26 +0100

    Update demo

---
+++ precache-manifest.179c72f51692314a529acaf64914ed6f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53dc22acf84ecab566899deed83a4d42",
+    "url": "../rodant-demo/index.html"
+  },
+  {
+    "revision": "2405ba0a96c8bbc9495d",
+    "url": "../rodant-demo/static/css/3.619f2ad9.chunk.css"
+  },
+  {
+    "revision": "60ecaf46b3a7cd64e0dc",
+    "url": "../rodant-demo/static/css/main.d2d868ee.chunk.css"
+  },
+  {
+    "revision": "786ab8091b7c6b3f8788",
+    "url": "../rodant-demo/static/js/0.4b691af5.chunk.js"
+  },
+  {
+    "revision": "2405ba0a96c8bbc9495d",
+    "url": "../rodant-demo/static/js/3.455f5fd9.chunk.js"
+  },
+  {
+    "revision": "60ecaf46b3a7cd64e0dc",
+    "url": "../rodant-demo/static/js/main.dc3058e6.chunk.js"
+  },
+  {
+    "revision": "bbb157e62ec6934018ff",
+    "url": "../rodant-demo/static/js/runtime-main.eeb97671.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "../rodant-demo/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "../rodant-demo/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "../rodant-demo/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "../rodant-demo/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "../rodant-demo/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rodant-demo/precache-manifest.13bb93c0842d8da282ba2040677851f9.js"
+  "../rodant-demo/precache-manifest.179c72f51692314a529acaf64914ed6f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rodant-demo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("../rodant-demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
